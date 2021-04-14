2b67ac9b5010dd49ac8435079ccddae612806f09
Squeak-Paradox <69602381+Squeak-Paradox@users.noreply.github.com>
2021-04-14 16:57:17 +0100

    changes contact form submission

--- script/contactForm.js
+++ script/contactForm.js
@@ -1,5 +1,15 @@
 const contactForm = document.querySelector('.footer_contactForm')
 
-// contactForm.addEventListener('submit', (event) => {
-//     event.preventDefault()
-// })
+contactForm.addEventListener('submit', handleSubmit)
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    let myForm = document.getElementById('pizzaOrder');
+    let formData = new FormData(myForm)
+    fetch('/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString()
+    }).then(() => console.log('Form successfully submitted')).catch((error) =>
+      alert(error))
+  }
