472641d307a8fa8727a27899ae80deccf7558772
Squeak-Paradox <69602381+Squeak-Paradox@users.noreply.github.com>
2021-04-14 18:16:07 +0100

    video library functionality

--- script/filmLibrary.js
+++ script/filmLibrary.js
@@ -5,11 +5,49 @@ let libraryOpen = false
 
 libraryToggle.addEventListener('click', () => {
     const heightChange = libraryOpen ? '0' : '300px'
-    const overflowChange = libraryOpen ? 'hidden' : 'scroll'
     const iconRotate = libraryOpen ? '0' : '45deg'
 
-    libraryBody.style.overflowY = overflowChange
     libraryBody.style.height = heightChange
     libraryToggleIcon.style.transform = `rotate(${iconRotate})`
     libraryOpen = !libraryOpen
 }, {passive: true})
+
+const filmData = [
+    {
+        thumbnailSrc: 'http://placekitten.com/150/100',
+        title: 'Showreel 2021',
+        youtubeSrc: 'https://www.youtube.com/embed/Dqf0QgZLeuM'
+    },
+    {
+        thumbnailSrc: 'http://placekitten.com/200/300',
+        title: 'Do or Die CastMeComp',
+        youtubeSrc: 'https://www.youtube.com/embed/IRTh4F4kw1U'
+    },
+    {
+        thumbnailSrc: 'http://placekitten.com/100/100',
+        title: 'Showreel 2021',
+        youtubeSrc: 'https://www.youtube.com/embed/Dqf0QgZLeuM'
+    }
+]
+const videoPlayer = document.getElementById('videoPlayer-media')
+const library = document.querySelector('.film_library_body')
+
+filmData.forEach(film => {
+    const item = document.createElement('div')
+    item.classList.add('film_library_item')
+
+    const image = document.createElement('img')
+    image.classList.add('film_library_item_img')
+    image.setAttribute('src', film.thumbnailSrc)
+    image.setAttribute('alt', film.title)
+
+    const title = document.createElement('p')
+    title.textContent = film.title
+
+    item.append(image, title)
+    item.addEventListener('click', () => {
+        videoPlayer.setAttribute('src', film.youtubeSrc)
+    })
+
+    library.append(item)
+})
