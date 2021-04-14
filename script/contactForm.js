7d34806f761800841626d53846cb70007901a596
Squeak-Paradox <69602381+Squeak-Paradox@users.noreply.github.com>
2021-04-14 16:13:06 +0100

    contact form debug test

--- script/contactForm.js
+++ script/contactForm.js
@@ -1,5 +1,5 @@
 const contactForm = document.querySelector('.footer_contactForm')
 
-contactForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-})
+// contactForm.addEventListener('submit', (event) => {
+//     event.preventDefault()
+// })
