6243a99fd91e243e575cf3d36fde186568cb53b3
Squeak-Paradox <69602381+Squeak-Paradox@users.noreply.github.com>
2021-04-15 21:48:18 +0100

    Home Content CMS fields

--- script/indexContent.js
+++ script/indexContent.js
@@ -1,7 +1,10 @@
 const aboutContentContainer = document.querySelector('.about_body')
+const currentlyAppearingContainer = document.querySelector('.projects_current')
 
-fetch('../content/about.json')
+fetch('../content/home.json')
     .then(response => response.json())
     .then(data => {
         aboutContentContainer.textContent = data.aboutBody
+        currentlyAppearingContainer.textContent = data.currentlyAppearing
+        console.log(data.recentJobs)
     })
