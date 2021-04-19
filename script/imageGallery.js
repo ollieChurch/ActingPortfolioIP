aa54e379086bc957fb6d3ebd214566934a86f9d5
Squeak-Paradox <69602381+Squeak-Paradox@users.noreply.github.com>
2021-04-19 19:12:38 +0100

    update image gallery CMS

--- script/imageGallery.js
+++ script/imageGallery.js
@@ -1,75 +1,22 @@
-const imageData = [
-    {
-        src: 'yellowHeadshot.jpeg',
-        alt: 'headshot of Isobelle Pippin',
-        desc: 'Isobelle Pippin headshot',
-        copy: 'Robin Savage'
-    },
-    {
-        src: 'blackHeadshot.jpeg',
-        alt: 'headshot of Isobelle Pippin',
-        desc: 'Isobelle Pippin headshot',
-        copy: 'Robin Savage'
-    },
-    {
-        src: 'blueHeadshot.jpeg',
-        alt: 'headshot of Isobelle Pippin',
-        desc: 'Isobelle Pippin headshot',
-        copy: 'Robin Savage'
-    },
-    {
-        src: 'grayHeadshot.jpeg',
-        alt: 'headshot of Isobelle Pippin',
-        desc: 'Isobelle Pippin headshot',
-        copy: 'Robin Savage'
-    },
-    {
-        src: 'greenHeadshot.jpeg',
-        alt: 'headshot of Isobelle Pippin',
-        desc: 'Isobelle Pippin headshot',
-        copy: 'Robin Savage'
-    },
-    {
-        src: 'sleevelessHeadshot.jpeg',
-        alt: 'headshot of Isobelle Pippin',
-        desc: 'Isobelle Pippin headshot',
-        copy: 'Robin Savage'
-    },
-    {
-        src: 'whiteHeadshot.jpeg',
-        alt: 'headshot of Isobelle Pippin',
-        desc: 'Isobelle Pippin headshot',
-        copy: 'Robin Savage'
-    },
-    {
-        src: 'yellowHeadshot.jpeg',
-        alt: 'a cat',
-        desc: 'This is a cat 7',
-        copy: 'Puss in Boots'
-    },
-    {
-        src: 'yellowHeadshot.jpeg',
-        alt: 'a cat',
-        desc: 'This is a cat 8',
-        copy: 'Puss in Boots'
-    }
-]
-
 const galleryContainer = document.querySelector('.gallery_imagesContainer')
 
-imageData.forEach(image => {
-    const newContainer = document.createElement('div')
-    const newImage = document.createElement('img')
+fetch('../content/media.json')
+    .then(response => response.json())
+    .then(data => {
+        data.imageGallery.forEach(image => {
+            const newContainer = document.createElement('div')
+            const newImage = document.createElement('img')
 
-    newImage.setAttribute('src', `images/${image.src}`)
-    newImage.setAttribute('alt', image.alt)
+            newImage.setAttribute('src', `images/${image.src}`)
+            newImage.setAttribute('alt', image.desc)
 
-    newContainer.classList.add('gallery_img')
-    newContainer.append(newImage)
-    newContainer.addEventListener('click', () => showImage(image), {passive: true})
+            newContainer.classList.add('gallery_img')
+            newContainer.append(newImage)
+            newContainer.addEventListener('click', () => showImage(image), {passive: true})
 
-    galleryContainer.append(newContainer)
-})
+            galleryContainer.append(newContainer)
+        })
+    })
 
 const popUpClose = document.querySelector('.popUp_close')
 const popUpContainer = document.querySelector('.popUpContainer')
@@ -79,7 +26,7 @@ const popUpCopy = document.querySelector('.copyright_name')
 
 function showImage(image) {
     popUpImg.setAttribute('src', `images/${image.src}`)
-    popUpImg.setAttribute('alt', image.alt)
+    popUpImg.setAttribute('alt', image.desc)
     popUpDesc.textContent = image.desc
     popUpCopy.textContent = image.copy
     popUpContainer.style.display = 'block'
