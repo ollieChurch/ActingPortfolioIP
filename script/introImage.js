a2ff6fdd69069699c2091ad1ba24178e108a2175
Squeak-Paradox <69602381+Squeak-Paradox@users.noreply.github.com>
2021-04-22 11:21:11 +0100

    add headshot CMS functionality

--- script/introImage.js
+++ script/introImage.js
@@ -1,22 +1,23 @@
 const mainImg = document.querySelector('.intro_img')
-const headshotData = [
-    'yellowHeadshot',
-    'blackHeadshot',
-    'blueHeadshot',
-    'grayHeadshot',
-    'greenHeadshot',
-    'sleevelessHeadshot',
-    'whiteHeadshot'
-]
 let count = 0
+
+fetch('../content/headshots.json')
+    .then(response => response.json())
+    .then(data => {
+        const {headshotList} = data
+        mainImg.setAttribute('src', headshotList[count].image)
+
+        if (headshotList.length > 1) {
+            setInterval(() => {
+                mainImg.style.opacity = '0'
+                count = count + 1 < headshotData.length ? count + 1 : 0
+                setTimeout(() => {
+                    mainImg.setAttribute('src', headshotList[count].image)
+                    mainImg.style.opacity = '1'
+                }, 1250)
+            }, 10000)
+        }
+    })
 
 mainImg.addEventListener('click', () => location.assign('media.html'))
 
-setInterval(() => {
-    mainImg.style.opacity = '0'
-    count = count + 1 < headshotData.length ? count + 1 : 0
-    setTimeout(() => {
-        mainImg.setAttribute('src', `images/${headshotData[count]}.jpeg`)
-        mainImg.style.opacity = '1'
-    }, 1250)
-}, 10000)
