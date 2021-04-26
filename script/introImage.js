921a1880467b3aca407b93236dfd6d6bae603b64
Squeak-Paradox <69602381+Squeak-Paradox@users.noreply.github.com>
2021-04-26 15:16:19 +0100

    Update introImage.js

--- script/introImage.js
+++ script/introImage.js
@@ -6,15 +6,14 @@ fetch('../content/headshots.json')
     .then(data => {
         const {headshotList} = data
         mainImg.setAttribute('src', headshotList[count].image)
-        console.log(headshotList)
 
         if (headshotList.length > 1) {
             setInterval(() => {
                 mainImg.style.opacity = '0'
                 count = count + 1 < headshotList.length ? count + 1 : 0
                 setTimeout(() => {
+                    mainImg.onload = () => { mainImg.style.opacity = '1' }
                     mainImg.setAttribute('src', headshotList[count].image)
-                    setTimeout(() => { mainImg.style.opacity = '1' }, 250)
                 }, 1250)
             }, 10000)
         }
