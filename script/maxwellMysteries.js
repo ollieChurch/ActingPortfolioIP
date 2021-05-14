337ff077b7fa568d7dbca9a677fa06fb9ea1d25c
Squeak-Paradox <69602381+Squeak-Paradox@users.noreply.github.com>
2021-05-14 13:33:38 +0100

    projects CMS clean up

--- script/maxwellMysteries.js
+++ script/maxwellMysteries.js
@@ -3,6 +3,7 @@ const productsContainer = document.querySelector('.maxwellMysteries_products')
 fetch('../content/creativeProjects.json')
     .then(response => response.json())
     .then(data => {
+        console.log(data.maxwellMysteries)
         data.maxwellMysteries.forEach(product => {
             const newContainer = document.createElement('div')
             newContainer.classList.add('product')
@@ -17,7 +18,7 @@ fetch('../content/creativeProjects.json')
 
             const media = product.useVideoTrailer ? document.createElement('iframe') : document.createElement('img')
             const mediaSrc = product.useVideoTrailer ? product.videoSrc : product.src
-            product.useVideoTrailer && media.classList.add('videoPlayer')
+            media.classList.add('videoPlayer')
             media.setAttribute('width', '100%')
             media.setAttribute('height', '315')
             media.setAttribute('src', mediaSrc)
@@ -27,48 +28,45 @@ fetch('../content/creativeProjects.json')
                 media.setAttribute('allow', 'accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture')
                 media.setAttribute('allowfullscreen', true)
             } 
-
-            if (!product.gameInDevelopment) {
-                const story = document.createElement('p')
-                story.classList.add('product_story')
-                story.textContent = product.gameStory
-
-                const description = document.createElement('p')
-                description.classList.add('product_description')
-                description.textContent = product.gameDescription
             
-                const requirementsHeading = document.createElement('p')
-                requirementsHeading.classList.add('product_requirements_heading')
-                requirementsHeading.textContent = 'What You Need:'
-                const requirementsList = document.createElement('ul')
-                requirementsList.classList.add('product_requirements_list')
+            const developmentText = document.createElement('p')
+            developmentText.classList.add('product_story')
+            developmentText.textContent = 'This game is currently in development'
+
+            const story = document.createElement('p')
+            story.classList.add('product_story')
+            story.textContent = product.gameStory
+
+            const description = document.createElement('p')
+            description.classList.add('product_description')
+            description.textContent = product.gameDescription
+        
+            const requirementsHeading = document.createElement('p')
+            requirementsHeading.classList.add('product_requirements_heading')
+            requirementsHeading.textContent = 'What You Need:'
+            const requirementsList = document.createElement('ul')
+            requirementsList.classList.add('product_requirements_list')
+            if (product.requirementsList) {
                 product.requirementsList.forEach(item => {
                     const newListItem = document.createElement('li')
                     newListItem.textContent = item.requirement
                     requirementsList.append(newListItem)
                 })
-                
-                const finalLine = document.createElement('p')
-                finalLine.textContent = 'Play as a team or take on the case as a lone detective. The choice is yours.'
-
-                const price = document.createElement('h4')
-                price.classList.add('product_price')
-                price.textContent = `Hours of fun for just £${product.price}`
-
-                if (product.productID) {
-                    const buyBtn = document.createElement('div')
-                    buyBtn.classList.add('pp-product-widget-container')
-                    buyBtn.setAttribute('data-product', product.productID)
-                    buyBtn.setAttribute('data-show', 'button')
-                    buyBtn.setAttribute('data-button-label', 'Buy It Now!')
-                    buyBtn.setAttribute('data-button-classes', 'product_btn')
-                }
-            } else {
-                const developmentText = document.createElement('p')
-                developmentText.classList.add('product_story')
-                developmentText.textContent = 'This game is currently in development'
             }
             
+            const finalLine = document.createElement('p')
+            finalLine.textContent = 'Play as a team or take on the case as a lone detective. The choice is yours.'
+
+            const price = document.createElement('h4')
+            price.classList.add('product_price')
+            price.textContent = `Hours of fun for just £${product.price}`
+
+            const buyBtn = document.createElement('div')
+            buyBtn.classList.add('pp-product-widget-container')
+            buyBtn.setAttribute('data-product', product.productID)
+            buyBtn.setAttribute('data-show', 'button')
+            buyBtn.setAttribute('data-button-label', 'Buy It Now!')
+            buyBtn.setAttribute('data-button-classes', 'product_btn')
 
             newContainer.append(
                 title, 
@@ -76,7 +74,9 @@ fetch('../content/creativeProjects.json')
                 media
             )
 
-            if (!product.gameInDevelopment) {
+            if (product.gameInDevelopment) {
+                newContainer.append(developmentText)
+            } else {
                 newContainer.append(
                     story, 
                     description, 
@@ -86,8 +86,6 @@ fetch('../content/creativeProjects.json')
                     price,
                     buyBtn
                 )
-            } else {
-                newContainer.append(developmentText)
             }
 
             productsContainer.append(newContainer)
